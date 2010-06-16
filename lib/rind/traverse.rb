4ce8a60ff5c7da943a92faff8a80a0ed56bc098f
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2010-06-16 01:17:35 -0500

    adding tests and correcting traversal function output when there is no return value

--- lib/rind/traverse.rb
+++ lib/rind/traverse.rb
@@ -11,26 +11,22 @@ module Traverse
 			ancestors.push(node)
 		end
 
-		if ancestors.empty?
-			nil
-		else
-			path.nil? ? ancestors : ancestors.filter(path)
-		end
+		(path.nil? or ancestors.empty?) ? ancestors : ancestors.filter(path)
 	end
 
 	# Creates a Rind::Nodes list of all descendants. If an Xpath
 	# is provided it will only return the nodes that match.
 	def descendants(path = nil)
+		descendants = Rind::Nodes.new()
 		if self.respond_to? :children and not self.children.empty?
-			descendants = Rind::Nodes.new(self.children)
+			descendants.push(*self.children)
 
 			self.children.each do |child|
 				child_descendants = child.descendants
 				descendants.push(*child_descendants) if not child_descendants.nil?
 			end
-
-			path.nil? ? descendants : descendants.filter(path)
 		end
+		(path.nil? or descendants.empty?) ? descendants : descendants.filter(path)
 	end
 
 	# Returns the first descendant node. If an Xpath is provided
@@ -60,7 +56,7 @@ module Traverse
 	# the first one that matches.
 	def next(path = nil)
 		siblings = self.next_siblings(path)
-		siblings.first if not siblings.nil?
+		siblings.first
 	end
 
 	# Creates a Rind::Nodes list of all siblings that follow the
@@ -77,7 +73,7 @@ module Traverse
 	# the first one that matches.
 	def prev(path = nil)
 		siblings = self.prev_siblings(path)
-		siblings.last if not siblings.nil?
+		siblings.last
 	end
 
 	# Creates a Rind::Nodes list of all siblings that proceed the
@@ -100,6 +96,6 @@ module Traverse
 	# it will return the first one that matches.
 	def up(path = nil)
 		ancestors = self.ancestors(path)
-		ancestors.first if not ancestors.nil?
+		ancestors.first
 	end
 end
--- test/traverse_test.rb
+++ test/traverse_test.rb
@@ -9,49 +9,59 @@ class TraverseTest < Test::Unit::TestCase
   def test_ancestors
     assert_equal(@root.down('f').ancestors, [@root.down('d'), @root.down('c'), @root.down('b'), @root])
     assert_equal(@root.down('f').ancestors('c'), [@root.down('c')])
+		assert_equal(@root.ancestors, [])
   end
 
   def test_descendants
     assert_equal(@root.down('c').descendants, [@root.down('d'), @root.down('h'), @root.down('i'), @root.down('e'), @root.down('f'), @root.down('g'), @root.down('j')])
     assert_equal(@root.descendants('f'), [@root.down('f')])
+		assert_equal(@root.down('e').descendants, [])
   end
 
   def test_down
     assert_equal(@root.down.name, 'b')
     assert_equal(@root.down('f').name, 'f')
+		assert_nil(Rind::Element.new().down)
   end
 
   def test_next
     assert_same(@root.down('f').next, @root.down('g'))
     assert_same(@root.down('e').next('g'), @root.down('g'))
+		assert_nil(@root.down.next)
   end
 
   def test_next_siblings
     assert_equal(@root.down('f').next_siblings, [@root.down('g')])
     assert_equal(@root.down('e').next_siblings('g'), [@root.down('g')])
+		assert_equal(@root.down.next_siblings, [])
   end
 
   def test_parent
     assert_same(@root.down.parent, @root)
+		assert_nil(@root.parent)
   end
 
   def test_prev
     assert_same(@root.down('f').prev, @root.down('e'))
     assert_same(@root.down('g').prev('e'), @root.down('e'))
+		assert_nil(@root.down.prev)
   end
 
   def test_prev_siblings
     assert_equal(@root.down('f').prev_siblings, [@root.down('e')])
     assert_equal(@root.down('g').prev_siblings('e'), [@root.down('e')])
+		assert_equal(@root.down.prev_siblings, [])
   end
 
   def test_siblings
     assert_equal(@root.down('f').siblings, [@root.down('e'), @root.down('g')])
     assert_equal(@root.down('f').siblings('e'), [@root.down('e')])
+		assert_equal(@root.down.siblings, [])
   end
 
   def test_up
     assert_same(@root.down('f').up, @root.down('d'))
     assert_same(@root.down('f').up('b'), @root.down('b'))
+		assert_nil(@root.up)
   end
 end
