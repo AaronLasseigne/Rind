194cb3becb8452d2797c3d9156c2dc7dd7edba90
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2010-06-18 01:40:50 -0500

    adding is_leaf? and is_root? functions to nodes

--- lib/rind/node.rb
+++ lib/rind/node.rb
@@ -4,4 +4,16 @@ module Node
 	include Xpath
 
 	attr_accessor :parent
+
+	def is_leaf?
+		if self.respond_to? :children
+			self.children.empty? ? true : false
+		else
+			true
+		end
+	end
+
+	def is_root?
+		self.parent.nil? ? true : false
+	end
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -2,12 +2,24 @@ require 'test/unit'
 require 'rind'
 
 class NodeTest < Test::Unit::TestCase
-  def test_parent
-		node1 = Rind::Element.new
-		node2 = Rind::Element.new
-		node1.children.push(node2)
+	def setup
+		@node1 = Rind::Element.new
+		@node2 = Rind::Element.new
+		@node1.children.push(@node2)
+	end
 
-    assert_same(node2.parent, node1)
-		assert_nil(node1.parent)
+  def test_parent
+    assert_same(@node2.parent, @node1)
+		assert_nil(@node1.parent)
   end
+
+	def test_is_leaf?
+		assert(!@node1.is_leaf?)
+		assert(@node2.is_leaf?)
+	end
+
+	def test_is_root?
+		assert(@node1.is_root?)
+		assert(!@node2.is_root?)
+	end
 end
